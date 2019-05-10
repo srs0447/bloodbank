283fd8a8718b24d27b2d232b91517f1d88da182b
R.S.Chauhan <srs0447@gmail.com>
2019-05-10 14:42:02 +0530

    Add db

--- bloodbank.sql
+++ bloodbank.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 19, 2019 at 09:17 PM
+-- Generation Time: May 10, 2019 at 11:10 AM
 -- Server version: 10.1.19-MariaDB
 -- PHP Version: 5.6.28
 
@@ -34,6 +34,37 @@ CREATE TABLE `banks` (
   `passw` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `banks`
+--
+
+INSERT INTO `banks` (`id`, `bb_name`, `email`, `mob`, `passw`) VALUES
+(2, 'rs', 'rs@rs.com', 85858, '5555'),
+(3, 'rs', 'rs@rs.com', 85858, '5555');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bloods`
+--
+
+CREATE TABLE `bloods` (
+  `bid` int(11) NOT NULL,
+  `name` varchar(5) NOT NULL,
+  `unit` int(11) NOT NULL,
+  `bbid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bloods`
+--
+
+INSERT INTO `bloods` (`bid`, `name`, `unit`, `bbid`) VALUES
+(1, 'a', 2, 2),
+(2, 'v', 4, 2),
+(3, 'a', 2, 0),
+(4, 'v', 4, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -49,24 +80,23 @@ CREATE TABLE `users` (
   `pass` varchar(255) NOT NULL,
   `mobile` varchar(12) NOT NULL,
   `bloodg` varchar(3) NOT NULL,
-  `area` varchar(255) NOT NULL,
+  `pin` int(11) NOT NULL,
   `post` varchar(255) NOT NULL,
-  `block` varchar(255) NOT NULL,
-  `subDisrtict` varchar(255) NOT NULL,
+  `blok` varchar(255) NOT NULL,
+  `subd` varchar(255) NOT NULL,
   `district` varchar(255) NOT NULL,
-  `state` varchar(255) NOT NULL,
+  `states` varchar(255) NOT NULL,
   `country` varchar(255) NOT NULL,
-  `status` varchar(3) NOT NULL DEFAULT '''0'''
+  `status` enum('none','need','available','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `dob`, `pass`, `mobile`, `bloodg`, `area`, `post`, `block`, `subDisrtict`, `district`, `state`, `country`, `status`) VALUES
-(1, 'rs', 'chauhan', 'srs@gmail.com', '2017-07-20', 'rschauhan', '9161060447', 'AB+', '', '', '', '', '', '', '', ''''),
-(2, 'rs', 'chauhan', 'srsrs@gmaial.com', '2019-04-18', '123456', '1234567890', 'AB+', '', '', '', '', '', '', '', ''''),
-(3, 'Anoop ', 'Kumar', 'kanoop640@gmail.com', '1995-03-15', 'ashok7869', '8953997805', 'A-', '', '', '', '', '', '', '', '''');
+INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `dob`, `pass`, `mobile`, `bloodg`, `pin`, `post`, `blok`, `subd`, `district`, `states`, `country`, `status`) VALUES
+(7, 'rs', 'chauhhan', '', '1996-08-31', 'rschauhan', '', 'A-', 0, '', '', '', '', '', '', 'none'),
+(8, 'Ramcharan', 'Chauhan', 'rsrsq@rs.com', '1999-02-22', '123', '8737822006', 'A-', 2761233, 'tandadih', 'tarawa', 'mehnagar', 'azamagrh', 'uttar pradesh', 'india', 'none');
 
 --
 -- Indexes for dumped tables
@@ -77,6 +107,13 @@ INSERT INTO `users` (`id`, `fname`, `lname`, `email`, `dob`, `pass`, `mobile`, `
 --
 ALTER TABLE `banks`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `bloods`
+--
+ALTER TABLE `bloods`
+  ADD PRIMARY KEY (`bid`),
+  ADD KEY `bbid` (`bbid`);
 
 --
 -- Indexes for table `users`
@@ -92,12 +129,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `banks`
 --
 ALTER TABLE `banks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `bloods`
+--
+ALTER TABLE `bloods`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
